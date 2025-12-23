d4e0f5e9fba2f3415225e7618d125046b6fd2d24
elmakhtarlaarif <mokhtarlaarif54@gmail.com>
2025-12-23 15:30:00 +0100

    initialisation du projet

---
+++ database/tables.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE Sport_Event_Manager;
+use Sport_Event_Manager;
+CREATE TABLE Club (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    ville VARCHAR(100),
+    date_creation DATE
+);
+
+CREATE TABLE Equipe (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    jeu VARCHAR(50),
+    club_id INT,
+    FOREIGN KEY (club_id) REFERENCES Club(id) ON DELETE CASCADE
+);
+
+CREATE TABLE Joueur (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pseudo VARCHAR(100) NOT NULL,
+    role VARCHAR(50),
+    salaire DECIMAL(10,2),
+    equipe_id INT,
+    FOREIGN KEY (equipe_id) REFERENCES Equipe(id) ON DELETE CASCADE
+);
+
+CREATE TABLE Tournoi (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titre VARCHAR(100) NOT NULL,
+    cashprize DECIMAL(15,2),
+    format VARCHAR(50),
+    date_tournoi DATE
+);
+
+CREATE TABLE `Match` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    score_a INT,
+    score_b INT,
+    equipeA_id INT,
+    equipeB_id INT,
+    tournoi_id INT,
+    gagnant_id INT,
+    FOREIGN KEY (equipeA_id) REFERENCES Equipe(id),
+    FOREIGN KEY (equipeB_id) REFERENCES Equipe(id),
+    FOREIGN KEY (tournoi_id) REFERENCES Tournoi(id) ON DELETE CASCADE,
+    FOREIGN KEY (gagnant_id) REFERENCES Equipe(id)
+);
+
+CREATE TABLE Sponsor (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    contribution DECIMAL(15,2),
+    tournoi_id INT,
+    FOREIGN KEY (tournoi_id) REFERENCES Tournoi(id) ON DELETE CASCADE
+);
